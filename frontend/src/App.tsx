48bcaa11db86b81735d2adf70e0a3252c4c2ce95
dima991911 <192837465ds>
2021-02-22 00:16:42 +0200

    implement ant design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Layout } from "antd";
 
 import './App.css';
 
@@ -8,16 +9,30 @@ import { Home, Login, SignUp } from "./pages";
 
 import { store } from "./store";
 
+const { Content, Footer, Header, Sider } = Layout;
+
 function App() {
     return (
         <Provider store={store}>
-            <Router>
-                <div>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/login" exact component={Login} />
-                    <Route path="/signup" exact component={SignUp} />
-                </div>
-            </Router>
+            <Layout>
+                <Sider>Left sidebar</Sider>
+
+                <Layout className="main-section">
+                    <Header className="site-layout-background">Header</Header>
+                    <Layout className="content-section">
+                        <Content>
+                            <div className="site-layout-background">
+                                <Router>
+                                    <Route path="/" exact component={Home} />
+                                    <Route path="/login" exact component={Login} />
+                                    <Route path="/signup" exact component={SignUp} />
+                                </Router>
+                            </div>
+                        </Content>
+                    </Layout>
+                    <Footer>Footer</Footer>
+                </Layout>
+            </Layout>
         </Provider>
     );
 }
